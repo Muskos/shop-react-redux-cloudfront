d61754f1884345ef48b5ea0f121548b832922a10
Aliaksei Makas <a.d.makas@gmail.com>
2024-04-07 23:54:15 +0200

    feat: switched to monorepo with nx for future future functions structure

--- apps/shop/vite.config.ts
+++ apps/shop/vite.config.ts
@@ -7,6 +7,10 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: "../../dist",
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
